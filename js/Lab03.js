5edbc3e99c729473dc6b1a353cf68fb003903ae8
AbdielP <abdiel.pinzonc@gmail.com>
2021-05-08 01:12:23 -0500

    Calcular palindromo base 2 y 10.

--- js/Lab03.js
+++ js/Lab03.js
@@ -1,8 +1,37 @@
 class Lab03 {
     constructor() {
         this.resultadoPrimo = document.getElementById('resultadop');
+        this.resultadoPaldr = document.getElementById('resultadopal');
         this.formp = document.getElementById('primoForm');
-        // this.primos(this.formp);
+        this.formpal = document.getElementById('palForm');
+
+        this.palindromo(this.formpal);
+        this.primos(this.formp);
+    }
+
+    palindromo(form) {
+        let num_temporal, rem, final = 0;
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let numero = Number(document.getElementById('palindromo').value);
+            num_temporal = numero;
+            while(numero > 0){
+                rem = numero%10;
+                numero = parseInt(numero/10);
+                final = final*10+rem;
+                console.log(final)
+            }
+
+            if(final === num_temporal){
+                console.log(`El número ${num_temporal} es palíndromo.`);
+                this.resultadoPaldr.innerHTML = `El número ${num_temporal} es palíndromo.`;
+                final = 0;
+            } else {
+                console.log(`El número ${num_temporal} NO ES palíndromo.`)
+                this.resultadoPaldr.innerHTML = `El número ${num_temporal} NO ES palíndromo.`;
+                final = 0;
+            }
+        });
     }
 
     primos(form) {
